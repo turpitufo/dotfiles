81244c075532208151ec0607c710e6a0ed3c5d2d
d <tufanyesil@protonmail.com>
2026-07-14 01:38:52 +0200

    nu to fish change, nu keeps crashing in long nix rebuilds

--- flake.nix
+++ flake.nix
@@ -2,12 +2,11 @@
   description = "flake for pNix";
 
   inputs = {
-
     nixos-06cb-009a-fingerprint-sensor = {
       url = "github:viktor-grunwaldt/t480-fingerprint-nixos";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    
+
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     nur.url = "github:nix-community/NUR";
@@ -15,48 +14,23 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    #plasma-manager = {
-    #url = "github:nix-community/plasma-manager";
-    #inputs.nixpkgs.follows = "nixpkgs";
-    #inputs.home-manager.follows = "home-manager";
-    #};
-
-#    calibData = {
-#      url = "path:./hosts/pNix/calib-data.bin";
-#    };
-
-
   };
 
-
-  outputs = { self, 
-  nixpkgs, 
-  home-manager, 
-  chaotic, 
-  nur, 
-  nixos-06cb-009a-fingerprint-sensor,
-#  calibData,
-  ... }@inputs: {
-
+  outputs = { self, nixpkgs, home-manager, chaotic, nur, nixos-06cb-009a-fingerprint-sensor, ... }@inputs: {
     nixosConfigurations.pNix = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit self inputs; };
       modules = [
         ./hosts/pNix/configuration.nix
         home-manager.nixosModules.home-manager
-	{
+        {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.d = import ./hosts/pNix/home.nix;
           home-manager.extraSpecialArgs = { inherit inputs self; };
-	}
-	nixos-06cb-009a-fingerprint-sensor.nixosModules."06cb-009a-fingerprint-sensor"
-
-     ];
+        }
+        nixos-06cb-009a-fingerprint-sensor.nixosModules."06cb-009a-fingerprint-sensor"
+      ];
     };
   };
-
-
-
 }
-
---
+++ home/programs/fish.nix
@@ -0,0 +1,67 @@
+{ config, pkgs, lib, self, ... }:
+{
+  programs = {
+    fish = {
+      enable = true;
+
+      # Fish shell configuration
+      shellInit = ''
+        # Disable welcome message
+        set -g fish_greeting
+
+        # History settings
+        set -g history_merge_mode all
+        set -g history_time_format "%Y-%m-%d %H:%M:%S"
+
+        # Completion settings
+        set -g complete_case_sensitive no
+        set -g complete_keep_right yes
+
+        # Prompt - using starship
+        function fish_prompt
+          starship prompt
+        end
+      '';
+
+      shellAbbrs = {
+        vi = "nvim";
+        vim = "nvim";
+        nano = "nvim";
+        rb = "sudo nixos-rebuild switch --flake /home/d/pNix#pNix";
+        rebuild = "sudo nixos-rebuild switch --flake /home/d/pNix#pNix";
+        fu = "cd /home/d/pNix && nix flake update";
+        sync = "cd /home/d/pNix && nix flake update && sudo nixos-rebuild switch --flake .#pNix";
+	ls = "eza";
+      };
+
+      shellAliases = {
+        ll = "ls -la";
+        la = "ls -a";
+      };
+
+      # Fish plugins (using fisher)
+      plugins = [
+        # Starship is enabled separately via home-manager
+        # fisher plugins can be added here if needed
+      ];
+    };
+
+    # Starship configuration (same as nushell config)
+    starship = {
+      enable = true;
+      settings = {
+        add_newline = true;
+        character = {
+          success_symbol = "[➜](bold green) ";
+          error_symbol = "[✗](bold red) ";
+        };
+      };
+    };
+
+    # Disable carapace nushell integration since we're using fish
+    carapace = {
+      enable = true;
+      enableNushellIntegration = false;
+    };
+  };
+}
